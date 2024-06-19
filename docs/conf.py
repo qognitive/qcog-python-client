475e02202a3f6d3c4f58a68135f31a9c0f406720
jeff berger <jeff@qognitive.io>
2024-06-19 12:37:43 -0400

    Theme change, include PDF versions

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ author = "Qognitive Inc"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.duration",
@@ -29,7 +30,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 

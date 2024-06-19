573f87deac597b3a3a3df7c2d797393ebb41ef8a
jeff berger <jeff@qognitive.io>
2024-06-19 12:22:58 -0400

    sphinx doc page

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "qcog-python-client"
+copyright = "2024, Qognitive Inc"
+author = "Qognitive Inc"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.duration",
+    "sphinx.ext.autosummary",
+    "autoapi.extension",
+    "sphinx_paramlinks",  # from sqlalchemy: needed to stop :paramref: errors
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+
+# -- AutoAPI configuration ---------------------------------------------------
+autoapi_type = "python"
+autoapi_dirs = ["../qcog_python_client"]
---
+++ docs/source/index.rst
@@ -0,0 +1,14 @@
+Welcome to qcog-python-client's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`

263d3b1d68585b50acc2a93443842c0f994b87a7
Sebastien Roy <sebastien.roy@qognitive.io>
2024-07-12 11:15:16 -0400

    build schema before build package

--- examples/train_test.py
+++ examples/train_test.py
@@ -24,6 +24,7 @@ def main():
     hsm = (
         QcogClient.create(
             token=API_TOKEN,
+            version="0.0.71",
         )
         .ensemble(
             operators=["X", "Y", "Z"],

87bc5c2ca9343233a1329d687aced928e9fcb1e1
Francesco Vertemati <francesco.vertemati@qognitive.io>
2024-09-25 14:44:10 -0400

    remove unused import

--- qcog_python_client/qcog/__init__.py
+++ qcog_python_client/qcog/__init__.py
@@ -1,7 +1,2 @@
-from pydantic import BaseModel
-
 from qcog_python_client.qcog.qcogasync import AsyncQcogClient  # noqa
 from qcog_python_client.qcog.qcogsync import QcogClient  # noqa
-
-# Globally disable Pydantic protected namespaces warning
-BaseModel.model_config["protected_namespaces"] = ()
